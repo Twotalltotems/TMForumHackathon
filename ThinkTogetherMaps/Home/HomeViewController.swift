aae0b5465df30eb079044b118fedffc202bd021a
Evian <evian.zhao@twotalltotems.com>
2016-07-11 15:33:58 -0700

    Setup navigation bar

--- ThinkTogetherMaps/Home/HomeViewController.swift
+++ ThinkTogetherMaps/Home/HomeViewController.swift
@@ -12,8 +12,24 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupNavigationBar()
+    }
+    
+    private func setupNavigationBar() {
         
         title = "Main Screen"
+        
+        let leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Search, target: self, action: #selector(HomeViewController.searchAction(_:)))
+        navigationItem.leftBarButtonItem = leftBarButtonItem
+        
+        let rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .Organize, target: self, action: #selector(HomeViewController.settingsAction(_:)))
+        navigationItem.rightBarButtonItem = rightBarButtonItem
+    }
+    
+    func searchAction(sender: UIButton) {
+    }
+    
+    func settingsAction(sender: UIButton) {
     }
 }
 
